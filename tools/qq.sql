118856fd214defaada431102173eff2a63efe956
mohammed Mustafa <mohmoustafa@malomatia.com>
2026-08-24 11:38:18 +0300

    chore: remove obsolete tools directory

--- tools/qq.sql
+++
@@ -1,5 +0,0 @@
-SELECT USER AS db_user, SYS_CONTEXT('USERENV','CURRENT_SCHEMA') AS cur_schema FROM DUAL
-----
-SELECT owner, object_type, status FROM all_objects WHERE object_name = 'XXHMC_SND_PHONE_TYPE_V'
-----
-SELECT owner, object_name, object_type, status FROM all_objects WHERE object_name LIKE 'XXHMC_SND_PHONE%' ORDER BY 2,3
